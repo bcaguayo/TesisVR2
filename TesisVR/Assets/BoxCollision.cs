de349caac4a00ba38f24a838a1f070b6d1942724
bcaguayo <saguayo@seas.upenn.edu>
2021-12-16 17:42:58 -0300

    Working Version 1

---
+++ TesisVR/Assets/BoxCollision.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCollision : MonoBehaviour
+{
+    [SerializeField] private GameObject box;
+    [SerializeField] private bool chosen;
+    public roomReset room;
+	private Renderer boxRenderer;
+    // public bool discovered;
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        string collider = collisionInfo.collider.name;
+        // boxboxRenderer = GetComponent<Renderer>();
+        room.Collision(collider, box, chosen);    
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
